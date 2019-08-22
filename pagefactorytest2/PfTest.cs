32d4f8674bb22c0aa842addd88de66ccb4ded818
bp <bartlomiej.pilch.1981@gmail.com>
2019-08-22 12:13:51 +0200

    commtit

---
+++ pagefactorytest2/PfTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pagefactorytest2
+{
+    class PfTest
+    {
+        IWebDriver driver;
+        [FindsBy(How = How.XPath, Using = ("//a[text()='Teams'"))]
+        public IWebElement teamsButton;
+
+        public PfTest()
+        {
+            Iniatialize();
+        }
+        public void Iniatialize()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://nhl.com");
+
+
+            SeleniumExtras.PageObjects.PageFactory.InitElements(driver, this);
+
+        }
+
+        ~PfTest()
+        {
+            driver.Quit();
+        }
+
+
+    }
+}
---
+++ pagefactorytest2/test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pagefactorytest2
+{
+    class test
+    {
+
+        [Test]
+        public void nhlMainPageTest()
+        {
+            PfTest test = new PfTest();
+            test.teamsButton.Click();
+        }
+    }
+}
